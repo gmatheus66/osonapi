053ac2907c9fabfe835e7396277585c6848ef98f
Matheus G <gmatheus66@hotmail.com>
2022-02-27 01:24:11 -0300

    Start project

---
+++ src/main/java/com/api/oson/OsonApplication.java
@@ -0,0 +1,13 @@
+package com.api.oson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/oson/OsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.oson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
